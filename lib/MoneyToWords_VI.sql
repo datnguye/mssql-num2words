3c09bcb9378be861283b1642d4b14ee6cb13c8a5
datnguye <datnguyen.it09@gmail.com>
2020-08-29 12:26:51 +0700

    pseudo code

--- lib/MoneyToWords_VI.sql
+++ lib/MoneyToWords_VI.sql
@@ -1,4 +1,4 @@
---======================================================
+﻿--======================================================
 -- Usage:	Lib: MoneyToWords in Vietnamese
 -- Notes:	
 -- History:
@@ -12,6 +12,37 @@ RETURNS NVARCHAR(MAX)
 AS 
 BEGIN
 	DECLARE @Result NVARCHAR(MAX) = N''
+	DECLARE @vTo19 TABLE (ID int)
+
+	/*
+		Convert_Integer_2text
+			If n < 100
+				convert number xx
+			Elif n < 1000
+				convert number xxx
+			Else
+				for (didx, dval) in ((v - 1, 1000 ** v) for v in range(len(denom))):
+				if dval > val:
+					mod = 1000 ** didx
+					lval = val // mod
+					r = val - (lval * mod)
+
+					ret = self._convert_nnn(lval) + u' ' + denom[didx]
+					if 99 >= r > 0:
+						ret = self._convert_nnn(lval) + u' ' + denom[didx] + u' lẻ'
+					if r > 0:
+						ret = ret + ' ' + self.vietnam_number(r)
+					return ret
+
+		Main_func:
+			Use Convert_Integer_2text
+			Fragement by 1000 then to call Convert_Integer_2text
+
+		Splitted by dot: first.sec
+		Result = Main_func(first) + phẩy + Main_func(sec)
+
+	*/
+
     RETURN @Result
 END
 /*
