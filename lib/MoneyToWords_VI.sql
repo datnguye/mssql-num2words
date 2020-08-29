546912f0b6ab61f95281715f2f26516c1e433fee
datnguye <datnguyen.it09@gmail.com>
2020-08-29 20:31:47 +0700

    updates

--- lib/MoneyToWords_VI.sql
+++ lib/MoneyToWords_VI.sql
@@ -7,13 +7,34 @@
 --======================================================
 DROP FUNCTION IF EXISTS MoneyToWords_VI
 GO
-CREATE FUNCTION MoneyToWords_VI(@BaseNumber DECIMAL(17,2))
+CREATE FUNCTION MoneyToWords_VI(@Number DECIMAL(17,2))
 RETURNS NVARCHAR(MAX)
 AS 
 BEGIN
-	DECLARE @Result NVARCHAR(MAX) = N''
-	DECLARE @vTo19 TABLE (ID int)
+	DECLARE @Result		NVARCHAR(MAX) = N''
+	DECLARE @tTo19		TABLE (Num int NOT NULL, Nam nvarchar(255) NOT NULL)
+	INSERT 
+	INTO	@tTo19 (Num, Nam)
+	VALUES	(0,N'không'),(1,N'một'),(2,N'hai'),(3,N'ba'),(4,N'bốn'),(5,N'năm'),(6,N'sáu'),(7,N'bảy'),(8,N'tám'),(9,N'chín'),
+			(10,N'mười'),(11,N'mười một'),(12,N'mười hai'),(13,N'mười ba'),(14,N'mười bốn'),(15,N'mười lăm'),(16,N'mười sáu'),(17,N'mười bảy'),(18,N'mười tám'),(19,N'mười chín')
+	
+	DECLARE @FirstWord		nvarchar(255) = N'mốt'
+	DECLARE @OddWord		nvarchar(255) = N'lẻ'
+	DECLARE @TensWord		nvarchar(255) = N'mươi'
+	DECLARE @HundredWord	nvarchar(255) = N'trăm'
+	DECLARE @ThousandWord	nvarchar(255) = N'nghìn'
+	DECLARE @MillionWord	nvarchar(255) = N'triệu'
+	DECLARE @BillionWord	nvarchar(255) = N'tỷ'
+	DECLARE @DotWord		nvarchar(255) = N'phẩy'
 
+	DECLARE @vMainNum DECIMAL(17,2) = FLOOR(@Number)
+	DECLARE @vDecimalNum DECIMAL(17,2) = (@Number - FLOOR(@Number)) * 100
+	DECLARE @vDecimalWords NVARCHAR(255)
+
+	IF @vDecimalNum <> 0
+		SET @vDecimalWords = dbo.MoneyToWords_VI(@vDecimalNum)
+
+	SET @Result = 'xxx' + COALESCE(' '+@DotWord+' '+@vDecimalWords, '')
 	/*
 		Convert_Integer_2text
 			If n < 100
@@ -36,7 +57,7 @@ BEGIN
 
 		Main_func:
 			Use Convert_Integer_2text
-			Fragement by 1000 then to call Convert_Integer_2text
+			Fragement by xxx then to call Convert_Integer_2text
 
 		Splitted by dot: first.sec
 		Result = Main_func(first) + phẩy + Main_func(sec)
@@ -46,5 +67,5 @@ BEGIN
     RETURN @Result
 END
 /*
-	SELECT dbo.MoneyToWords_VI(255)
+	SELECT dbo.MoneyToWords_VI(255.56)
 */
